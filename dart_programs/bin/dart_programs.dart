df8f5be97225d7f14315f6c2312f1c0be94bba59
flutter99 <82260050+flutter99@users.noreply.github.com>
2022-12-09 09:35:27 +0500

    2nd way to run dart program

--- dart_programs/bin/dart_programs.dart
+++
@@ -1,5 +0,0 @@
-import 'package:dart_programs/dart_programs.dart' as dart_programs;
-
-void main() {
-  print('Hello world');
-}
--- dart_programs/test/dart_programs_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_programs/dart_programs.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
